500917c42a47042ed663d735317e1c7eeba9af11
RISHI PRASAD <rishiprasad022@gmail.com>
2026-08-01 22:27:58 +0530

    Time: 0 ms (100%), Space: 9.8 MB (50.11%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        unordered_map <char,int> freq;
+        for(char ch:s){
+            freq[ch]++;
+        }
+        for(char ch:t){
+            freq[ch]--;
+        }
+        for(auto i:freq){
+            if(i.second!=0)
+                return false;
+        }
+        return true;
+    }
+};
